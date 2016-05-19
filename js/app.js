de0a4764f1d2d79014a109a45f4e6c7503cdc7ca
abelmaniquis <abelmaniquis@gmail.com>
2016-05-19 03:19:35 +0000

    static directions

--- js/app.js
+++ js/app.js
@@ -8,20 +8,48 @@ function addstop(){
 }
 
 function initMap(){
-    var latvar = 44.540;
-    var lngvar = -78.546;
+    var latvar = 32.7157;
+    var lngvar = -117.1611;
     var mapDiv = document.getElementById('map');
         var map = new google.maps.Map(mapDiv,{
-            center:{lat:44.540,lng:-78.546},
+            center:{lat:32.7157,lng:-117.1611},
             zoom: 8
         });
-    /*
-    This creates a new Map.
-    */
+        
+        var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    draggable: true,
+    map: map,
+    panel: document.getElementById('right-panel')
+  });
+        
+      displayRoute('7527 Celata Court, San Diego, CA', '9500 Gilman Drive, La Jolla, CA', directionsService,
+      directionsDisplay);
 }
 
-function displayRoute(){
-    //This shows the route on the map
+function displayRoute(origin, destination, service, display){
+
+  service.route({
+    origin: origin,
+    destination: destination,
+    waypoints:
+      [{
+          location: '7930 Park Village Rd, San Diego, CA, 92129'
+      },{
+          location: '8375 Entreken Way, San Diego, CA, 92129'
+      },{
+          location: '13500 Camino Del Sur, San Diego, CA, 92129'
+      }],
+    travelMode: google.maps.TravelMode.DRIVING,
+    avoidTolls: true
+  }, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+      display.setDirections(response);
+      } else {
+      alert('Could not display directions due to: ' + status);
+      }
+  });
+
 }
 
 function computeTotalDistance(){
